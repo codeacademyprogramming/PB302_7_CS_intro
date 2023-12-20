f15249640a3b14cd7e89116b164d248455d1777b
EAgha <41126467+EAghayev@users.noreply.github.com>
2023-12-20 21:54:57 +0400

    Add files via upload

---
+++ 20122023/Homework/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 10, 23, 25, 34, 56, 78 };
+
+            int n = 10;
+            int wantedIndex = -1;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == n)
+                {
+                    wantedIndex = i;
+                    break;
+                }
+            }
+
+
+            if (wantedIndex != -1)
+            {
+                Console.WriteLine($"Axtarilan eded {wantedIndex} index-de yerlesir");
+            }
+            else
+            {
+                Console.WriteLine("axtarilan eded tapilmadi");
+            }
+        }
+    }
+}
